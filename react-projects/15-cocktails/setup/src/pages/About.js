9e565407d513d5fe5aa23fedee38f7484b944212
shwetadokhe99@gmail.com <dokheshweta9@gmail.com>
2023-05-16 23:44:20 +0530

    update

---
+++ react-projects/15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+        adipisci molestias eligendi, natus sit doloremque, itaque soluta at
+        omnis repellendus laboriosam nesciunt atque neque, assumenda rerum enim
+        facilis expedita sequi commodi cumque aspernatur est magni possimus! Ea
+        amet iste suscipit?
+      </p>
+    </section>
+  );
+};
+
+export default About;
